2ce6626a3b103f87042f5e93f48610687fdc58a5
nicolettevibes <nicole_zhangzitong@outlook.com>
2024-06-27 23:16:25 +0800

    nicole finished extra credit

--- main.py
+++ main.py
@@ -15,3 +15,15 @@ list_of_cookies = cookie_shop.bake_cookies(filepath)
 # open the cookie shop with the cookies read from the file
 # this must run the rest of the program as documented
 cookie_shop.run_shop(list_of_cookies)
+
+import cookie_shop_extra_credit
+
+filepath = Path("data/cookies.csv")
+cookies = cookie_shop_extra_credit.load_cookies(filepath)
+nuts_allergy, gluten_allergy, diabetes = cookie_shop_extra_credit.welcome_message()
+    
+filtered_cookies = cookie_shop_extra_credit.filter_cookies(cookies, nuts_allergy == 'yes', gluten_allergy == 'yes', diabetes == 'yes')
+cookie_shop_extra_credit.display_cookies(filtered_cookies)
+order = cookie_shop_extra_credit.take_order(filtered_cookies)
+    
+cookie_shop_extra_credit.display_order_summary(order)
